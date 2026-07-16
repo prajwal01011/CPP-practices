382f9f90d7fbb1e713d11f2b9ce5402e545fc356
Prajwal Baskota <prajwalbaskota11@gmail.com>
2026-07-16 18:18:58 +0545

    changing program

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
+int add(int a,int b){
+    int z=a+b;
+    cout<<"The sum 0f"<<a<<"and"<<b<<"is"<<z;
+} 
+
 int main(){
-    cout<<"hello world";
+    int n1,n2;
+    cout<<"enter two numbers"<<endl;
+    cin>>n1>>n2;
+    add(n1,n2);
     return 0;
 }
