3057646078bf0b58ff802b822897a5fe0552cfc6
PrajwalBaskota <prajwalbaskota11@gmail.com>
2026-08-20 15:04:41 +0545

    more learning

--- oop.cpp
+++ oop.cpp
@@ -3,11 +3,29 @@
 using namespace std;
 
 
+class account{
+    private:
+    double balance;
+    string password;//data hiding
+
+    public:
+    string username;
+    string accountId;
+};
+
+
 class teacher{
+    private:
+    double salary;
+    public:
+    //constructor
+    teacher(){
+        cout<<"hi i am constructor"<<endl;
+    }
     //properties|attributes
     string name;
     string dept;
-    string salary;
+    
     string subject;
 
     //methods|member functions
@@ -15,12 +33,29 @@ class teacher{
 
         dept = newDept;
     }
+    //setter
+    void setSalary(double s){
+
+        salary=s;
+    }
+    //getter
+    double getSalary(){
+        return salary;
+    }
 
 };
 
 
 int main(){
-    teacher t1;// teacher obj
+    teacher t1;// teacher obj & constructor call
+    t1.name="prajwal";
+    t1.dept="c++";
+    t1.subject="computer science";
+    t1.setSalary(100000);
+
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+
 
 
 
