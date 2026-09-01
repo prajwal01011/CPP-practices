dfd9b7ac27e3ea39ae8f57d3f85452630f4726fc
PrajwalBaskota <prajwalbaskota11@gmail.com>
2026-09-01 14:46:12 +0545

    learning templets in function and in class and object

--- Templets.cpp
+++ Templets.cpp
@@ -8,13 +8,21 @@ void print(T value){
     cout<<"value :"<<value<<endl;
 }
 
+template<typename G,int NUM>
+class Array{
+    G array[NUM];
+public:
+int getsize()const{return NUM;}
+
+};
 
 
 int main(){
     print(1);
-    print(1.5);
-    print("skfjlasjf");
-    print('a');
+    print<float>(1.2f);
+    Array<int,5> array;
+    cout<<array.getsize()<<endl;
+    cin.get();
 
 
 
